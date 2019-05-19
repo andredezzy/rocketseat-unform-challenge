b6deb342ab5db6f69eb0bdc7f6a21cb2118667e8
André 'Dezzy <38265368+andredezzy@users.noreply.github.com>
2019-05-18 22:57:45 -0300

    Add files via upload

---
+++ precache-manifest.c5b4dddc159b5fefaf7cd4cdd82850bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5136cf49621baaf8fd2aa8417b23bfdf",
+    "url": "/rocketseat-unform-challenge/index.html"
+  },
+  {
+    "revision": "3e5b32c82db08cbd7486",
+    "url": "/rocketseat-unform-challenge/static/js/2.e4f527b7.chunk.js"
+  },
+  {
+    "revision": "dc7feb322a6c2bfadae3",
+    "url": "/rocketseat-unform-challenge/static/js/main.1ded54b0.chunk.js"
+  },
+  {
+    "revision": "ae2d8a3e3cdaddf18f72",
+    "url": "/rocketseat-unform-challenge/static/js/runtime~main.6992605a.js"
+  },
+  {
+    "revision": "a493208be2684dcd6130bd153f73750f",
+    "url": "/rocketseat-unform-challenge/static/media/delete-icon.a493208b.svg"
+  },
+  {
+    "revision": "ce6df6679775dcfc4a04c2512ea6421d",
+    "url": "/rocketseat-unform-challenge/static/media/github-icon.ce6df667.svg"
+  },
+  {
+    "revision": "bc056d3359a86ecbaa582f330f34ff12",
+    "url": "/rocketseat-unform-challenge/static/media/left-arrow-icon.bc056d33.svg"
+  },
+  {
+    "revision": "b33f38db6a35ada43833fb551d2c29be",
+    "url": "/rocketseat-unform-challenge/static/media/right-arrow-icon.b33f38db.svg"
+  },
+  {
+    "revision": "2ab974220388e264ed070280dc755249",
+    "url": "/rocketseat-unform-challenge/static/media/rocketseat-launch-logo.2ab97422.png"
+  },
+  {
+    "revision": "a1805e04b451522b6dd9b2db8217198c",
+    "url": "/rocketseat-unform-challenge/static/media/unform-logo.a1805e04.png"
+  }
+]);
